fa12cfe2844dc055767543d6e7a8a754ce0a84cd
Ethan Pham <ethansaddy@gmail.com>
2026-04-05 13:43:36 -0700

    Implement expose_writer_mem_view()

--- solution.py
+++ solution.py
@@ -286,6 +286,32 @@ class SharedBuffer(shared_memory.SharedMemory):
         self._validate_is_reader()
         raise NotImplementedError("TODO: implement SharedBuffer.jump_to_writer")
 
+    def _make_memory_views(self, size: int):
+        # Guard clause to save time
+        if size <= 0:
+            return memoryview(bytearray()), None, 0, False
+
+        readable_bytes = SharedBuffer.writer_position - self._position
+        # Clamp actual size
+        actual_size = min(size, readable_bytes)
+
+        # If buffer would overflow, split and wrap around
+        if self._position + actual_size > self.buffer_size:
+            split = True
+            # First part of memory view is at the end of the buffer
+            mv1 = self.buf[self._position:].toreadonly()
+
+            # Second part of memory view (wrapped around) is at beginning
+            # Subtract 1 since position is 0-indexed
+            remaining_bytes = actual_size - (self.buffer_size - self._position - 1)
+            mv2 = self.buf[0:remaining_bytes]
+        else:
+            split = False
+            mv1 = self.buf[self._position:self._position + actual_size]
+            mv2 = None
+
+        return mv1, mv2, actual_size, split
+
     def expose_writer_mem_view(self, size: int) -> RingView:
         """
         Return a writable view tuple for up to `size` bytes.
@@ -300,7 +326,7 @@ class SharedBuffer(shared_memory.SharedMemory):
         available rather than raising.
         """
         self._validate_is_writer()
-        raise NotImplementedError("TODO: implement SharedBuffer.expose_writer_mem_view")
+        return self._make_memory_views(size)
 
     def expose_reader_mem_view(self, size: int) -> RingView:
         """
@@ -310,24 +336,10 @@ class SharedBuffer(shared_memory.SharedMemory):
         are currently readable, clamp to the amount available rather than raising.
         """
         self._validate_is_reader()
-        readable_bytes = SharedBuffer.writer_position - self._position
-        # Clamp actual size
-        actual_size = min(size, readable_bytes)
-
-        # If buffer would overflow, split and wrap around
-        if self._position + actual_size > self.buffer_size:
-            split = True
-            # First part of memory view is at the end of the buffer
-            mv1 = self.buf[self._position:]
-
-            # Second part of memory view (wrapped around) is at beginning
-            # Subtract 1 since position is 0-indexed
-            remaining_bytes = actual_size - (self.buffer_size - self._position - 1)
-            mv2 = self.buf[0:remaining_bytes]
-        else:
-            split = False
-            mv1 = self.buf[self._position:self._position + actual_size]
-            mv2 = None
+        mv1, mv2, actual_size, split = self._make_memory_views(size)
+        mv1 = mv1.toreadonly()
+        if mv2 is not None:
+            mv2 = mv2.toreadonly()
 
         return mv1, mv2, actual_size, split
 
